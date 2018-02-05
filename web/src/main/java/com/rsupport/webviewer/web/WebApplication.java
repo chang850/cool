b843a52d1897848f628dd8b7fde1b573d2ad3ff8
Yun chang Hee <chyun@rsupport.com>
2018-02-05 16:39:00 +0900

    swaager 적용

--- web/src/main/java/com/rsupport/webviewer/web/WebApplication.java
+++ web/src/main/java/com/rsupport/webviewer/web/WebApplication.java
@@ -4,19 +4,63 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//Module 분리시 EntityScan 이 정상적으로 이루어지지 않고 이씀.......
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableWebMvc
+//@EnableWebMvc
+@EnableSwagger2
 @EnableJpaRepositories(basePackages = {"com.rsupport.webviewer.core.repository"})
 @EntityScan(basePackages = {"com.rsupport.webviewer.core.domain"})
 @ComponentScan(basePackages = {"com.rsupport.webviewer.web", "com.rsupport.webviewer.core"})
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    //내가 해야 할것은 url 이 아닌 controller 로 매핑
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rsupport.webviewer.web"))
+                .paths(PathSelectors.any())
+                .build();
+//                .groupName("greetings")   --> 임시 주석
+//                .apiInfo(apiInfo()) --> 복잡해지는 관계로 info 주석
+//                .select()
+//                .paths(regex("/greeting.*"))
+//                .build();
+
+
+//        .select()
+//                .apis(RequestHandlerSelectors.withClassAnnotation(ResponseBody.class))
+//                //.apis(RequestHandlerSelectors.basePackage("com.example.web.viewer"))
+//                .paths(PathSelectors.any())
+//                .build();
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger Title")
+                .description("Spring REST Sample with Swagger Description")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .contact("Niklas Heidloff")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+
     }
 }
--- web/src/main/java/com/rsupport/webviewer/web/viewer/ViewerController.java
+++ web/src/main/java/com/rsupport/webviewer/web/viewer/ViewerController.java
@@ -2,16 +2,19 @@ package com.rsupport.webviewer.web.viewer;
 
 import com.rsupport.webviewer.web.common.ServicePropertiesUtil;
 import com.rsupport.webviewer.web.common.WebViewerProperties;
+import com.rsupport.webviewer.web.swagger.Greeting;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +24,18 @@ public class ViewerController {
 
     private final WebViewerProperties webViewerProperties;
 
+    @ApiOperation(value = "뷰어 시작" ,tags = "changhee2")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "User's name", required = false,
+                    dataType = "String", paramType ="query", defaultValue = "Niklas")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Greeting.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
     @GetMapping("/")
     public String welcome(HttpServletRequest request, HttpServletResponse response, Model model) {
 
@@ -29,6 +44,26 @@ public class ViewerController {
         model.addAttribute("project1", webViewerProperties.getRole());
         model.addAttribute("project2", webViewerProperties.getProject().getName());
         return "viewer/start";
+    }
+
+
+    @ApiOperation(value = "해당게시판 조회", tags = "changhee")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "사용자 이름", required = false,
+                    dataType = "String", paramType ="query", defaultValue = "Niklas")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Greeting.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    @RequestMapping(method = RequestMethod.GET, value = "/greeting", produces = "application/json")
+    @ResponseBody
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        String template = "Hello, %s!";
+        return new Greeting(new AtomicLong().incrementAndGet(), String.format(template, name));
     }
 
     @GetMapping("/welcome")
